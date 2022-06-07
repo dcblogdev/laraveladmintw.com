3a638cee866f042b96df5ade3773843db99aebb8
David Carr <dave@dcblog.dev>
2022-06-07 03:30:22 +0100

    verification

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google-site-verification" content="uGqgWhXCrlQqLDyItEC-5OnbwrNGI48_tAwnB3JwKhg" />
         <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <script src="{{ mix('js/app.js') }}" defer></script>
