c3d0843d2dcefb66554eb20a234f55f5efd9decb
David Carr <dave@dcblog.dev>
2022-06-07 01:58:03 +0100

    removed demo link

--- resources/views/layouts/docs/header.blade.php
+++ resources/views/layouts/docs/header.blade.php
@@ -2,7 +2,7 @@
 
     <div class="relative pt-6">
 
-        <div class="max-w-screen-xl mx-auto px-4 py-4 sm:px-6 border-b-2 border-gray-50 dark:border-gray-800">
+        <div class="max-w-screen-xl px-4 py-4 mx-auto border-b-2 sm:px-6 border-gray-50 dark:border-gray-800">
 
             <nav class="relative flex items-center justify-between sm:h-10 md:justify-center">
                 <div class="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
@@ -13,9 +13,9 @@
                         </a>
                     </div>
 
-                    <div class="-mr-2 flex items-center md:hidden">
-                        <button type="button" @click="open = true" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" id="main-menu" aria-label="Main menu" aria-haspopup="true">
-                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <div class="flex items-center -mr-2 md:hidden">
+                        <button type="button" @click="open = true" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500" id="main-menu" aria-label="Main menu" aria-haspopup="true">
+                            <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
@@ -25,13 +25,12 @@
                 <div class="hidden md:block">
                     <a href="{{ url('/#features') }}" class="ml-10 font-medium text-gray-500 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-400">Features</a>
                     <a href="{{ url('docs') }}" class="ml-10 font-medium text-gray-500 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-400">Docs</a>
-                    <a href="https://demo.laraveladmintw.com" target="_blank" class="ml-10 font-medium text-gray-500 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-400">Demo</a>
                 </div>
 
             </nav>
         </div>
 
-        <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        <div class="absolute inset-x-0 top-0 p-2 transition origin-top-right transform md:hidden"
         x-transition:enter="transition ease-out duration-100 transform"
         x-transition:enter-start="opacity-0 scale-95"
         x-transition:enter-end="opacity-100 scale-100"
@@ -40,9 +39,9 @@
         x-transition:leave-end="opacity-0 scale-95"
         x-show="open">
             <div class="rounded-lg shadow-md">
-                <div class="rounded-lg bg-white dark:bg-gray-700 shadow-xs overflow-hidden" role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
+                <div class="overflow-hidden bg-white rounded-lg shadow-xs dark:bg-gray-700" role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
 
-                    <div class="px-5 pt-4 flex items-center justify-between">
+                    <div class="flex items-center justify-between px-5 pt-4">
 
                         <div>
                             <a href="{{ url('/') }}" class="text-4xl font-bold text-gray-800 dark:text-gray-200">
@@ -51,8 +50,8 @@
                         </div>
 
                         <div class="-mr-2">
-                            <button type="button" @click="open = false" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Close menu">
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <button type="button" @click="open = false" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500" aria-label="Close menu">
+                                <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </button>
@@ -61,9 +60,9 @@
                     </div>
 
                     <div class="px-2 pt-2 pb-3">
-                        <a href="{{ url('/#features') }}" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-500 dark:hover:text-gray-200 transition duration-150 ease-in-out" role="menuitem">Features</a>
-                        <a href="{{ url('docs') }}" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-500 dark:hover:text-gray-200 transition duration-150 ease-in-out" role="menuitem">Docs</a>
-                        <a href="https://demo.laraveladmintw.com" target="_blank" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-500 dark:hover:text-gray-200 transition duration-150 ease-in-out" role="menuitem">Demo</a>
+                        <a href="{{ url('/#features') }}" class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-500 dark:hover:text-gray-200" role="menuitem">Features</a>
+                        <a href="{{ url('docs') }}" class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-500 dark:hover:text-gray-200" role="menuitem">Docs</a>
+                        <a href="https://demo.laraveladmintw.com" target="_blank" class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:text-gray-900 focus:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-500 dark:hover:text-gray-200" role="menuitem">Demo</a>
                     </div>
 
                 </div>
