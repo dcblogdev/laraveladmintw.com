a9b899358165c02f99f75643a188ebe0f2c9cfaa
David Carr <dave@dcblog.dev>
2023-03-11 17:26:47 +0000

    production release

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,7 +8,7 @@
         <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <script src="{{ mix('js/app.js') }}" defer></script>
-        <script src="https://cdn.usefathom.com/script.js" data-site="PLJPRXLJ" defer></script>
+        <script src="https://cdn.usefathom.com/script.js" data-site="RERGCDUF" defer></script>
     </head>
     <body class="text-gray-900 dark:bg-gray-900">
             {{ $slot }}
